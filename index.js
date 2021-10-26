0ab06ed8397af45fb45d0275e2f03f4f3d965f33
Aron <hw1954@nyu.edu>
2021-10-26 09:39:34 -0400

    added heroku

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const indexRoute = require('./routes/index');
 
 app.use("/", indexRoute);
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:4000')
+    console.log(`Example app listening at http://localhost:${port}`)
 });
