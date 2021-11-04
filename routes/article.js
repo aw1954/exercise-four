2b29ea22ad7c2bee0f11661e740740761a749f71
Aron <hw1954@nyu.edu>
2021-11-04 09:19:20 -0400

    add all posts and single post query

--- routes/article.js
+++ routes/article.js
@@ -2,13 +2,30 @@ const express = require("express");
 
 const router = express.Router();
 
-//TODO: Hook up to firebase for a single article based on ID
+const firestore = require("firebase/firestore");
+
+const db = firestore.getFirestore();
+
+// Get single article by ID
+// accessible at /article/:id
+router.get("/:id", (req, res) => {
+    const postId = req.params.id;
+    const blogpost = firestore.getDoc(
+        firestore.doc(db, "blogposts", postId
+    ));
+    blogpost
+        .then((response) => {
+            const post = response.data();
+            if (post) return res.send(post);
+            return res.send(`no doc... sorry`);
+            })
+        .catch((error) => {
+            res.send(`No doc... sorry`);
+        });
+});
 
 router.get("/", (req, res) => {
-    res.send(`
-        <h1>Individual Article</h1>
-        <p>A single article will go here...</h1>
-    `);
+    res.send(`Please include an ID`);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,30 @@
 const express = require("express");
 // Middleware to allow for routing on the node server
 const router = express.Router();
-// Require Firebase
-const firebase = require("firebase/firestore/lite"); // Fix this...
+// Require Firestore
+const firestore = require("firebase/firestore"); 
 // Initialize Firebase Database
-const db = firebase.getFirestore(); // Fix this...
-// Reference to the blogposts collection 
-const blogposts = db.collection("blogposts"); // Fix this...
-
+const db = firestore.getFirestore();
 //Get all articles from firebase
 router.get("/", (req, res) => {
+    const blogposts = firestore.getDocs(
+        firestore.collection(db, "blogposts")
+    );
     // Create empty array
-    const blogpostsArray = []
-    // TODO: Get blogposts JSON from Firebase
+    const blogpostsArray = [];
 
-    // Push document from blogposts into the blogposts array...
-    res.send(blogpostsArray);
+    blogposts
+        .then((response) => {
+            response.forEach((doc) => {
+                // Push document into array every time the query loops over
+                blogpostsArray.push(doc.data());
+            });
+            return res.send(blogpostsArray);
+        })
+        .catch(function (error) {
+            console.log("Error:", error);
+            return res.send(error);
+        });
 });
 
 module.exports = router;
